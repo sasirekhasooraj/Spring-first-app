6a9f13b50cef7de1164782f56aa87c7eacb0abc3
ssasirekha <ssasirekha@gmail.com>
2021-10-26 10:16:48 +0530

    Initial commit

---
+++ src/main/java/com/example/springfirstapp/SpringFirstAppApplication.java
@@ -0,0 +1,20 @@
+package com.example.springfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringFirstAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFirstAppApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
